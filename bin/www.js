6a9332731891009abab71a2b69e4b68da3d81c31
ttcom <1902305073@qq.com>
2020-05-10 23:47:27 +0800

    Addsomthing

--- bin/www.js
+++ bin/www.js
@@ -6,3 +6,27 @@ const server = http.createServer(serverHandle);
 server.listen(3000,()=>{
     console.log('listening on 3000')
 })
+
+const mysql = require('mysql');
+
+//创建连接对象
+const con = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password: '123456',
+    port:'3306',
+    database:'blog'
+})
+
+//开始连接
+con.connect()
+
+//执行sql语句
+const sql = 'select * from user;'
+con.query(sql,(err, result) =>{
+    if(err){
+        console.error(err)
+        return
+    }
+    console.log(result)
+})
